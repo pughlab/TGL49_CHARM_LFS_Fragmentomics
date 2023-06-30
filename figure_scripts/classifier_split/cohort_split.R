2a6bfc6bf7daf2a3aadc409408724ffb3c693bd1
derekwong90 <78445989+derekwong90@users.noreply.github.com>
2023-06-30 15:01:55 -0700

    Update cohort_split.R

--- figure_scripts/classifier_split/cohort_split.R
+++ figure_scripts/classifier_split/cohort_split.R
@@ -1,13 +1,13 @@
 library(tidyverse)
 
 ### Read in sample lists and format
-data_samples <- read.delim("/Users/derekwong/OneDrive - UHN/Post-Doc/CHARM_Project/LFS/samples/sample_list.txt")
+data_samples <- read.delim("sample_list.txt")
 
 data_samples <- data_samples[!(data_samples$sWGS == ""), ]
 exclude <- c("TGL49_0025_Cf_U_PE_321_WG", "TGL49_0035_Cf_U_PE_310_WG", "TGL49_0041_Cf_U_PE_317_WG", "TGL49_0209_Cf_U_PE_373_WG")
 data_samples <- data_samples[!(data_samples$sWGS %in% exclude), ]
 
-healthy_samples <- read.delim("/Users/derekwong/Library/CloudStorage/OneDrive-UHN/Post-Doc/Healthy_control_cohorts/CHARM_HBC/sample_list.txt")
+healthy_samples <- read.delim("hbc/sample_list.txt")
 healthy_samples <- healthy_samples[!(healthy_samples$sWGS == ""), ]
 
 negatives <- data_samples[data_samples$cancer_status == "negative", ]
@@ -50,7 +50,7 @@ samples <- bind_rows(data.frame(sWGS = neg_test$sWGS,
                                 split = "validation"))
 
 ### Save sample list
-write.table(samples, file.path("/Users/derekwong/My Drive/Post-Doc/CHARM/LFS/LFS_fragment/figures/classifier_split", "samples_split.txt"), sep = "\t", row.names = FALSE)
+write.table(samples, file.path("samples_split.txt"), sep = "\t", row.names = FALSE)
 
 
 
