0ced0486b25488697eb9f3dc7710a94ba7cea6e8
derekwong90 <78445989+derekwong90@users.noreply.github.com>
2023-06-30 15:00:05 -0700

    Update classifier_nucleosome.R

--- figure_scripts/classifier_split/classifier_nucleosome.R
+++ figure_scripts/classifier_split/classifier_nucleosome.R
@@ -4,11 +4,11 @@ library(tidyverse)
 library(pROC)
 
 ### Set variables
-source("/Users/derekwong/My Drive/Post-Doc/CHARM/LFS/LFS_fragment/figures/classifier/FuncClassifier.R")
-class <- "/Users/derekwong/My Drive/Post-Doc/CHARM/LFS/LFS_fragment/figures/classifier_split/classifier.R"
-path <- "/Users/derekwong/OneDrive - UHN/Post-Doc/CHARM_Project/LFS/nucleosome_peaks"
-healthy_path <- "/Users/derekwong/OneDrive - UHN/Post-Doc/Healthy_control_cohorts/CHARM_HBC/nucleosome_peaks"
-outdir <- "/Users/derekwong/My Drive/Post-Doc/CHARM/LFS/LFS_fragment/figures/classifier_split"
+source("figure_scripts/classifier_split/FuncClassifier.R")
+class <- "figure_scripts/classifier_split/classifier.R"
+path <- "data/nucleosome_peaks"
+healthy_path <- "hbc/nucleosome_peaks"
+outdir <- ""
 
 ### Import data 
 data_peak <- read.delim(list.files(path, "CHARM_LFS_genome_peak_distances.txt", full.names = TRUE))
